24fbc8f6508d5546ab550e370dfbf39c05bb68a6
Naim Hosain <107548838+naimhosain@users.noreply.github.com>
2022-08-14 19:10:03 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    nav:true,
+    DelayNode
+    responsive:{
+        0:{
+            items:2
+        },
+        425:{
+            items:3
+        },
+        768:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+        
+    }
+})
